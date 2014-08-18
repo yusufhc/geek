32fb8d5581e69b0fda09729ac90ec29f0cd0b05e
yusufhc <yusuf.cochinwala@wipro.com>
2014-08-18 10:16:47 +0100

    Code movement
    Moving template code after apache service enablement

--- apache2/recipes/install_apache.rb
+++ apache2/recipes/install_apache.rb
@@ -1,12 +1,12 @@
 include_recipe 'apache2'
 
-template "/etc/httpd/conf.d/vhos.conf" do
+apache_site "default" do
+  enable true
+end
+
+template "/etc/httpd/conf.d/vhost.conf" do
 	source vhost_conf.erb
 	mode 0755
 	owner "root"
 	group "root"
-end
-
-apache_site "default" do
-  enable true
 end
