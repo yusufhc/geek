40f1c3797b704d94a99759bbf76ba8540856086d
yusufhc <yusuf.cochinwala@wipro.com>
2014-08-18 14:39:30 +0100

    Adding the remaining conf files
    Adding the remaining script files.

--- apache2/recipes/install_apache.rb
+++ apache2/recipes/install_apache.rb
@@ -1,7 +1,56 @@
 include_recipe 'apache2'
 
+template "/etc/httpd/conf.d/ssl.conf" do
+	source 'ssl.conf.erb'
+	mode 0755
+	owner "root"
+	group "root"
+end
+
+template "/etc/httpd/conf.d/urlredirect_aral.conf" do
+	source 'urlredirect_aral.conf.erb'
+	mode 0755
+	owner "root"
+	group "root"
+end
+
+template "/etc/httpd/conf.d/urlredirect_bp.conf" do
+	source 'urlredirect_bp.conf.erb'
+	mode 0755
+	owner "root"
+	group "root"
+end
+
+template "/etc/httpd/conf.d/urlredirect_castrol.conf" do
+	source 'urlredirect_castrol.conf.erb'
+	mode 0755
+	owner "root"
+	group "root"
+end
+
+template "/etc/httpd/conf.d/urlredirect_deutschebp.conf" do
+	source 'urlredirect_deutschebp.conf.erb'
+	mode 0755
+	owner "root"
+	group "root"
+end
+
+template "/etc/httpd/conf.d/urlredirect_http_default.conf" do
+	source 'urlredirect_http_default.conf.erb'
+	mode 0755
+	owner "root"
+	group "root"
+end
+
+template "/etc/httpd/conf.d/urlredirect_wildbeancafe.conf" do
+	source 'urlredirect_wildbeancafe.conf.erb'
+	mode 0755
+	owner "root"
+	group "root"
+end
+
 template "/etc/httpd/conf.d/vhost.conf" do
-	source 'vhost_conf.erb'
+	source 'vhost.conf.erb'
 	mode 0755
 	owner "root"
 	group "root"
